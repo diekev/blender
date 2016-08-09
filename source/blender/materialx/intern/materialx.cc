9b2303e0ef71064f0c0e4ae221bb81649d6a2fa5
Kévin Dietrich <kevin.dietrich@mailoo.org>
2016-08-10 01:21:38 +0200

    Cleanup.

--- source/blender/materialx/intern/materialx.cc
+++ source/blender/materialx/intern/materialx.cc
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Contributor(s): Esteban Tovagliari, Cedric Paille, Kevin Dietrich
+ * Contributor(s): Kevin Dietrich
  *
  * ***** END GPL LICENSE BLOCK *****
  */
