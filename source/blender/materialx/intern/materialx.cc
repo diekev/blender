dc024604dc7456c07faf45f8b904ed66a60f2ad0
Kévin Dietrich <kevin.dietrich@mailoo.org>
2016-08-10 00:53:04 +0200

    Add some test functions.

--- source/blender/materialx/intern/materialx.cc
+++ source/blender/materialx/intern/materialx.cc
@@ -24,6 +24,42 @@
 
 #include "../tinyxml2/tinyxml2.h"
 
+static void mtlx_add_geom_reference(tinyxml2::XMLDocument &doc,
+                                    tinyxml2::XMLElement *parent,
+                                    const char *collection_name,
+                                    const char *geom_str)
+{
+	tinyxml2::XMLElement *collection = doc.NewElement("collection");
+	collection->SetAttribute("name", collection_name);
+
+	tinyxml2::XMLElement *collectionadd = doc.NewElement("collectionadd");
+	collectionadd->SetAttribute("name", collection_name);
+	collectionadd->SetAttribute("geom", geom_str);
+
+	collection->InsertEndChild(collectionadd);
+
+	parent->InsertEndChild(collection);
+}
+
+static void mtlx_add_shader_reference(tinyxml2::XMLDocument &doc,
+                                      tinyxml2::XMLElement *parent,
+                                      const char *shader_name,
+                                      const char *shader_type,
+                                      const char *shaderprogram)
+{
+	tinyxml2::XMLElement *shader = doc.NewElement("shader");
+	/* Required atrributes. */
+	shader->SetAttribute("name", shader_name);
+	shader->SetAttribute("shadertype", shader_type);
+	shader->SetAttribute("shaderprogram", shaderprogram);
+	/* Optionnal attributes. */
+	shader->SetAttribute("aovset", "");
+	shader->SetAttribute("xpos", 0.0f);
+	shader->SetAttribute("ypos", 0.0f);
+
+	parent->InsertEndChild(shader);
+}
+
 void MTLX_export(const char *filename)
 {
 	tinyxml2::XMLDocument doc;
@@ -34,6 +70,31 @@ void MTLX_export(const char *filename)
 	root->SetAttribute("cms", "ocio");
 
 	doc.InsertEndChild(root);
+
+	mtlx_add_geom_reference(doc, root, "c_plastic", "/a/g1,/a/g2,/a/g5");
+	mtlx_add_geom_reference(doc, root, "c_metal", "/a/g3,/a/g4");
+	mtlx_add_geom_reference(doc, root, "c_headlampAssembly", "/a/lamp1/housing*Mesh");
+	mtlx_add_geom_reference(doc, root, "c_setgeom", "/b//*");
+
+	mtlx_add_shader_reference(doc, root, "srf1", "surface", "basic_surface");
+	mtlx_add_shader_reference(doc, root, "dsp1", "displacement", "noise_bump");
+
+#if 0
+	tinyxml2::XMLElement *image = doc.NewElement("image");
+	image->SetAttribute("name", "in1");
+	image->SetAttribute("type", "color3");
+	image->SetAttribute("colorspace", "lin_p3dci");
+
+	root->InsertEndChild(image);
+
+	tinyxml2::XMLElement *image_param = doc.NewElement("parameter");
+	image_param->SetAttribute("name", "file");
+	image_param->SetAttribute("type", "filename");
+	image_param->SetAttribute("type", "input1.tif");
+	image_param->SetAttribute("colorspace", "srgb_texture");
+
+	image->InsertEndChild(image_param);
+#endif
 
 	tinyxml2::XMLError eResult = doc.SaveFile(filename);
 
